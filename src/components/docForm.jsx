1eed4ff2c19844a8b4aabaab69a9d1edee5aaa84
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-10-15 18:05:47 +0530

    Submit Form functionality added--Data Displayed in the console

--- src/components/docForm.jsx
+++ src/components/docForm.jsx
@@ -8,6 +8,17 @@ const DocForm=()=>{
     //Destructuring Input Field Values
     const {name,speciality,experience,fees,qualification,location,lang,email,phno,gender,regno,specialization,supSpecialization}=formData;
 
+    //Handling Input Field Value Change
+    const handleChange=e=>{
+        setFormData({...formData,[e.target.name]:[e.target.value]});
+        // console.log(formData);
+    }
+
+    //Handling Form Submission
+    const handleSubmit=e=>{
+        console.log(formData);
+    }
+
     return(
         <React.Fragment>
             <Row>
@@ -20,57 +31,57 @@ const DocForm=()=>{
                     <Form>
                         <FormGroup>
                             <Label for="name">Doctor Name: </Label>
-                            <Input name="name" placeholder="Enter Doctor's Name"></Input>
+                            <Input name="name" placeholder="Enter Doctor's Name" value={name} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="speciality">Speciality: </Label>
-                            <Input name="speciality" ></Input>
+                            <Input name="speciality" value={speciality} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="experience">Experience: </Label>
-                            <Input name="experience" placeholder="Enter Doctor's Experience"></Input>
+                            <Input name="experience" placeholder="Enter Doctor's Experience" value={experience} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="fees">Consulting Fees: </Label>
-                            <Input name="fees" placeholder="Enter Doctor's Fees"></Input>
+                            <Input name="fees" placeholder="Enter Doctor's Fees" value={fees} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="qualification">Qualification: </Label>
-                            <Input name="qualification" placeholder="Enter Doctor's Qualification"></Input>
+                            <Input name="qualification" placeholder="Enter Doctor's Qualification" value={qualification} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="location">Location: </Label>
-                            <Input name="location" placeholder="Enter Doctor's Location"></Input>
+                            <Input name="location" placeholder="Enter Doctor's Location" value={location} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="lang">Languages: </Label>
-                            <Input name="lang" ></Input>
+                            <Input name="lang" value={lang} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="email">Email Id: </Label>
-                            <Input name="email" placeholder="Enter Doctor's Email"></Input>
+                            <Input name="email" placeholder="Enter Doctor's Email" value={email} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="phno">Contact Number: </Label>
-                            <Input name="phno" placeholder="Enter Doctor's Number"></Input>
+                            <Input name="phno" placeholder="Enter Doctor's Number" value={phno} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="gender">Gender: </Label>
-                            <Input name="gender" ></Input>
+                            <Input name="gender" value={gender} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="regno">Medical Registration Number: </Label>
-                            <Input name="regno" placeholder="Enter Doctor's Reistration Number"></Input>
+                            <Input name="regno" placeholder="Enter Doctor's Reistration Number" value={regno} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="specialization">Specialization: </Label>
-                            <Input name="specialization" placeholder="Enter Doctor's Specialization"></Input>
+                            <Input name="specialization" placeholder="Enter Doctor's Specialization" value={specialization} onChange={handleChange}></Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="supSpecialization">Super Specialization: </Label>
-                            <Input name="supSpecialization" placeholder="Enter Doctor's Super Specialization"></Input>
+                            <Input name="supSpecialization" placeholder="Enter Doctor's Super Specialization" value={supSpecialization} onChange={handleChange}></Input>
                         </FormGroup>
-                        <Button>Submit</Button>
+                        <Button onClick={handleSubmit}>Submit</Button>
                     </Form>
                 </Col> 
             </Row>
