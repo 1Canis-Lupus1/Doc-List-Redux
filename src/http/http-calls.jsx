c5d18d35bafaeaa77f31e231d882943e615a5d65
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-10-19 09:47:32 +0530

    Working on API call

--- src/http/http-calls.jsx
+++ src/http/http-calls.jsx
@@ -18,23 +18,3 @@ export const docData = () => {
         });
   });
 };
-
-// export const search = (input) => {
-//   return new Promise((resolve, reject) => {
-//     makeGetRequest(
-//         "https://api.giphy.com/v1/gifs/search",
-//         false,
-//         {
-//           "api_key" : "AupKJH29iQcqubiIMOhzdC9H6HVY1UsJ",
-//           "q" : input
-//         }
-//     )
-//     .then(res => {
-//         resolve(res.data);
-//       })
-//       .catch(e => {
-//         console.log("API call error: ", e);
-//         reject(e);
-//       });
-//   });
-// }
