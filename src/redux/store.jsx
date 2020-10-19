fb19a11e6214f849c65aaf47c982204d0d02b93c
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-10-20 03:22:18 +0530

    Pseudo Commit

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,4 +1,5 @@
 import {createStore,combineReducers} from 'redux';
 import {displayData} from './reducers';
 
+//conbineReducers in case we create 2 reducers for docForm and docTiming
 export default createStore(combineReducers({displayData}),window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_())
